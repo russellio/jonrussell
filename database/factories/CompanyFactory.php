e491eb27444b6946a4f595af1fca88a92b2744a0
Jon Russell <info@jonrussell.dev>
2026-05-28 10:46:05 -0400

    feat: add CompanyFactory for test fixtures

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CompanyFactory extends Factory
+{
+    protected $model = Company::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->company(),
+            'logo_src' => $this->faker->url(),
+            'logo_alt' => $this->faker->sentence(),
+            'logo_display_name' => $this->faker->boolean(),
+            'link' => $this->faker->url(),
+        ];
+    }
+}
